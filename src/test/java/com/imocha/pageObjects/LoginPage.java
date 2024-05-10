52dbdb42d255fc64df8ba459683a8cf5a2dfe2da
HarshR08 <harshal.rajguru08@gmail.com>
2024-05-10 18:39:12 +0530

    TC_ImageProctoredTest in progress

--- src/test/java/com/imocha/pageObjects/LoginPage.java
+++ src/test/java/com/imocha/pageObjects/LoginPage.java
@@ -8,48 +8,43 @@ import org.openqa.selenium.support.PageFactory;
 
 public class LoginPage {
 
-    WebDriver ldriver;
-    
-    public LoginPage(WebDriver rDriver){
-        ldriver = rDriver;
-        PageFactory.initElements(rDriver, this);
-    }
+	WebDriver ldriver;
 
-    @FindBy(xpath="//input[@id='EmailId']")
-    @CacheLookup
-    WebElement txtUserName;
+	public LoginPage(WebDriver rDriver) {
+		ldriver = rDriver;
+		PageFactory.initElements(rDriver, this);
+	}
 
-    @FindBy(xpath="//input[@id='Password']")
-    @CacheLookup
-    WebElement txtPassword;
-    
-    @FindBy(xpath="//input[@id='frmSubmitBtn']")
-    @CacheLookup
-    WebElement btnContinue;
+	@FindBy(xpath = "//input[@id='EmailId']")
+	@CacheLookup
+	WebElement txtUserName;
 
-    @FindBy(xpath="//input[@value='Login']")
-    @CacheLookup
-    WebElement btnLogin;
-    
-    public void setUserName(String uname)
-    {
-        txtUserName.sendKeys(uname);
-    }
+	@FindBy(xpath = "//input[@id='Password']")
+	@CacheLookup
+	WebElement txtPassword;
 
-    public void setPassword(String pwd)
-    {
-    	txtPassword.sendKeys(pwd);
-    }
-    
-    public void clickContinue()
-    {
-    	btnContinue.click();
-    }
+	@FindBy(xpath = "//input[@id='frmSubmitBtn']")
+	@CacheLookup
+	WebElement btnContinue;
 
-    public void clickSubmit()
-    {
-    	btnLogin.click();
-    }
-    
+	@FindBy(xpath = "//input[@value='Login']")
+	@CacheLookup
+	WebElement btnLogin;
+
+	public void setUserName(String uname) {
+		txtUserName.sendKeys(uname);
+	}
+
+	public void setPassword(String pwd) {
+		txtPassword.sendKeys(pwd);
+	}
+
+	public void clickContinue() {
+		btnContinue.click();
+	}
+
+	public void clickSubmit() {
+		btnLogin.click();
+	}
 
 }
